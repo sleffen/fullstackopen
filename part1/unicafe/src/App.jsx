80ef059517b9db876c7c2956f3b5eb431040cd73
Stefan Solvang <solvang.stefan@gmail.com>
2025-07-12 23:05:33 +0200

    unicafe and anecdotes 1.6 -1.14

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,25 +2,35 @@ import { useState } from 'react'
 
 const Header = () => <h1>give feedback</h1>
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => {
+
+  return (
+  <tr><td>{text}</td><td>{value}</td></tr>
+  )
+}
 
 const Statistics = ({all}) => {
   const {good, neutral, bad} = all
   const sum = good + neutral + bad
   const average = sum ? (good - bad) / sum : 0
-  const positive = sum ? ((good / sum) * 100)+'%' : 0
+  const positive = sum ? ((good / sum) * 100) : 0
 
   if(sum === 0 ) {return (<p>No feedback given</p>)}
   return (
-    <div>
-      <h1>Statistics</h1>
-      {Object.entries(all).map(([text, value]) => (
-        <StatisticLine key= {text} text={text} value={value} />
-      ))}
-      <StatisticLine key= 'total' text='total' value={sum} />
-      <StatisticLine key= 'average' text='average' value={average} />
-      <StatisticLine key= 'positive' text='positive' value={positive} />
-    </div>
+      // <h2>Statistics</h2>
+      <table>
+      <caption> <h2>Statistics</h2></caption>
+      <tbody>
+
+        {Object.entries(all).map(([text, value]) => (
+          <StatisticLine key= {text} text={text} value={value} />
+        ))}
+        <StatisticLine key= 'total' text='total' value={sum} />
+        <StatisticLine key= 'average' text='average' value={average.toFixed(1)} />
+        <StatisticLine key= 'positive' text='positive' value={positive.toFixed(1)+'%'} />
+      </tbody>
+      </table>
+
   )
 }
 
